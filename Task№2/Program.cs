c942cd32b2afcfa0d4f3c11f0af1f4caa3fef012
Vladislav <zomba4o4ek100@gmail.com>
2023-01-24 22:21:26 +0300

    Task second complete

---
+++ Task№2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
+int[] array = GetArray(6); 
+Console.WriteLine($"[{String.Join(";", array)}]");
+int[] array2 = Sort(array);
+Console.WriteLine($"[{String.Join(";", array2)}]");
+
+
+int[] GetArray (int size) 
+{ 
+    int[] array = new int[size]; 
+    for (int i = 0; i < size; i++) 
+    { 
+        array[i] = new Random().Next(0, 100);
+    } 
+    return array; 
+}
+
+int[] Sort (int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[min]) min = j;
+        }
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    }
+    return array;
+}
